7839922385601bc528eb34ca7c8f1db4b6096827
Ian Cover <iancover@gmail.com>
2022-05-16 17:25:30 -0500

    feat: Add basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('server...');
+// Modules
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+
+
+// App init & config
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.status(200).send('Hi there!');
+});
+
+app.listen((port = process.env.PORT), () => {
+  console.log(colors.cyan(`App listening on PORT: ${port}`));
+});
+
+// use 'ip' to view current IP for VPN/MongoDB conflicts

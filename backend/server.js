5fecb39db0272ba8310a43bf8781b1d203eb9144
Ian Cover <iancover@gmail.com>
2022-05-18 23:59:45 -0500

    feat(api): Import db obj/connect to db, import error handler

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,34 @@
 // Modules
 const express = require('express');
+const path = require('path');
 const colors = require('colors');
 require('dotenv').config();
+
+// Middleware
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+// MongoDB
+const connectDB = require('./config/db.js');
+connectDB();
 
 // App init
 const app = express();
 
 // App config
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // true = 'qs' library
 
 // Routes
 app.get('/', (req, res) => {
-  // res.status(200).send('T-Soup app');
   res.status(201).json({ message: 'Ticket-System Output and User Platform' });
 });
 
 app.use('/api/users', require('./routes/userRoutes'));
 
+// Handle errors
+app.use(errorHandler);
+
 // Server
 app.listen((port = process.env.PORT || 8000), () => {
-  console.log(colors.cyan(`App listening on PORT: ${port}`));
+  console.log(colors.cyan(`Server listening on PORT: ${port}`));
 });
-
-// use 'ip' to view current IP for VPN/MongoDB conflicts

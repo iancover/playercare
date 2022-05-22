deb7efd2d369a3d3b258c5f95195500ce6dc3a53
Ian Cover <iancover@gmail.com>
2022-05-22 13:46:07 -0500

    feat(ui): Add header and buttons to create or view tickets

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import { Link } from 'react-router-dom';
+import { RiFileAddFill, RiClipboardFill } from 'react-icons/ri';
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <header className='heading'>
+        <h1>Support Tickets</h1>
+        <p>Create a new ticket or view existing?</p>
+      </header>
+
+      <section className='form'>
+        <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+          <RiFileAddFill className='btn-icon' /> Create New
+        </Link>
+
+        <Link to='/tickets' className='btn btn-block'>
+          <RiClipboardFill className='btn-icon' /> View Existing
+        </Link>
+      </section>
+    </>
   );
 }
 

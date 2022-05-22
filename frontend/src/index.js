e6f877964de78d96dad328ab42b38e2a7516368d
Ian Cover <iancover@gmail.com>
2022-05-22 15:56:33 -0500

    feat(ui): Add/uncomment Redux store and Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 import App from './App';
 import './index.css';
@@ -13,9 +13,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
 

88f67ff6430b53fe43f12ad306837c76b5a9a9b0
Ian Cover <iancover@gmail.com>
2022-05-22 11:47:06 -0500

    Comment out store, remove default Redux counter folder

---
+++ frontend/src/app/store.js
@@ -0,0 +1,9 @@
+// Redux Store
+// import { configureStore } from '@reduxjs/toolkit';
+// import counterReducer from '../features/auth/authSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: authReducer,
+//   },
+// });

d46185d622e84721bacd592417281a55b5baf410
Ian Cover <iancover@gmail.com>
2022-06-03 15:54:37 -0500

    refactor(api): Redesign Schema for gaming industry
    Modify Schema to include issue categories to add a dropdown so users
    can select the type of technical issue, and add gaming consoles for
    platforms considering this is for a game compatible with all platforms.

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -8,14 +8,30 @@ const ticketSchema = mongoose.Schema(
       required: true,
       ref: 'User',
     },
-    product: {
+    platform: {
       type: String,
-      required: [true, 'Please select a product'],
-      enum: ['Nintendo', 'Xbox', 'Playstation', 'Sega'],
+      required: [true, 'Which platform are you gaming on?'],
+      enum: ['Nintendo Switch', 'Xbox Series X', 'PlayStation 5', 'PC Windows'],
+    },
+    issue: {
+      type: String,
+      required: [
+        true,
+        'Select the option that best identifies the type of issue.',
+      ],
+      enum: [
+        'Malfunction',
+        'Unresponsive',
+        'Connection',
+        'Memory',
+        'Graphics',
+        'Sounds',
+        'Other',
+      ],
     },
     description: {
       type: String,
-      required: [true, 'Please describe the issue.'],
+      required: [true, 'Please provide a short description of the issue.'],
     },
     status: {
       type: String,
@@ -30,3 +46,15 @@ const ticketSchema = mongoose.Schema(
 );
 
 module.exports = mongoose.model('Ticket', ticketSchema);
+
+// @todo: add small greyed out frame to list examples below to help user select option
+
+// malfunction - i.e. game lagging, stuttering, hanging, freezing, crashing, etc.
+// unresponsive - i.e. not starting, launching or responding to system interactions, etc.
+// connection - i.e. bad or no network connection, unable to sync with online account, etc.
+// memory - i.e. player progress not saving, loss of data from previous saved session, etc.
+// graphics - i.e. screen tearing, pixelation, glitches, image/color irregularities, etc.
+// sounds - i.e. no audio, low volume, music or sound effects delayed or distorted, etc.
+// other - i.e. in-game purchases, general gameplay question/feedback, unlisted issue, etc.
+
+// OPTIONAL: maybe add an option in case issue is with game card/cartridge itself???
